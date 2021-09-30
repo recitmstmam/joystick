3a023f604f568059e266728e3accd4f3824ced87
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-09-30 14:28:24 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ let yVal = 0
 let xVal = 0
 pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 basic.forever(function () {
-    xVal = pins.analogReadPin(AnalogPin.P1) * 11.9 / 14.24
-    yVal = pins.analogReadPin(AnalogPin.P2) * 11.9 / 14.25
+    xVal = pins.analogReadPin(AnalogPin.P1)
+    yVal = pins.analogReadPin(AnalogPin.P2)
     if (pins.digitalReadPin(DigitalPin.P8) == 0) {
         music.playTone(262, music.beat(BeatFraction.Quarter))
     }
